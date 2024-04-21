04f7c3f21771891d51831229e52266996284c562
Saritagomi <saritasbikhatod@gmail.com>
2024-04-22 01:03:24 +0530

    inital commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import StarsCanvas from "@/components/main/StarBackground";
+import Navbar from "@/components/main/Navbar";
+import Footer from "@/components/main/Footer";
+import icon from "@/app/s (1).png";
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sarita's Portfolio",
+  description: "This is my portfolio",
+  // icon: {icon},
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarsCanvas />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
